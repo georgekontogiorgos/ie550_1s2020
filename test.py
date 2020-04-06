e461dcb4a33313bed613afba65eedaf271d24521
george_kontogiorgos <gekontogiorgos@gmail.com>
2020-04-05 21:20:41 -0300

    First jupyter implementations

--- test.py
+++ test.py
@@ -1,3 +1,16 @@
-import plotly.graph_objects as go
-fig = go.Figure(data=go.Bar(y=[2, 3, 1]))
-fig.write_html('first_figure.html', auto_open=True)
+import matplotlib.pyplot as plt
+data = [
+    ('a', 4),
+    ('b', 5),
+    ('c', 5),
+    ('d', 4),
+    ('e', 2),
+    ('f', 5),
+]
+labels, y = zip(*data)
+
+x = range(len(y))
+plt.stem(x, y)
+plt.xticks(x, labels)
+plt.axis([-1, 6, 0, 6])
+plt.show()
