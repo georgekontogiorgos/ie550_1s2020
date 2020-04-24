126950e8b3a5f1c1a91e5a5c11a3506b95fb1e51
George Nicolas Kontogiorgos <george.kontogiorgos@lnls.br>
2020-04-23 22:49:06 -0300

    New figures to report

--- python/efc1.py
+++ python/efc1.py
@@ -4,22 +4,25 @@ from sistema_desconhecido import sistema_desconhecido
 
 ## Instantiate system identification class
 #   Maximum period = 100
-#   Minimum period = 2
-#   Periods in each frequency = 100
-sys_idn = system_identification(100, 2, 100)
+#   Minimum period = 1
+#   Periods in each frequency = 200
+sys_idn = system_identification(100, 1, 200)
 
 # Generate cosine "sweep" system input
 x = sys_idn.input_generator()
 
 # Get the system output from our input
+print("Exciting system to obtain its output")
 y = sistema_desconhecido(x)
+print("Finish")
 
 ## Get the frequency respose
-#   Eliminate first 99 periods per frequency for each frequency to eliminate transitory response
-H = sys_idn.output_processor(y, 99)
+#   Eliminate first 199 periods per frequency for each frequency to eliminate transitory response
+H = sys_idn.output_processor(y, 199)
 
 ## Plot all stuff ...
 #   Input
+print("Setting plot")
 plt.subplot(3, 1, 1)
 plt.stem(x, use_line_collection=True, basefmt=" ")
 plt.title('Input/output graphs')
@@ -40,6 +43,9 @@ plt.plot(sys_idn.frequencies_array(), H)
 plt.xlabel('Frequency [rad]')
 plt.ylabel('Amplitude [a.u.]')
 plt.grid()
+print("Finish")
 
+print("Saving figure to file")
 plt.savefig('frequency_response.png')
+print("Finish")
 plt.show()
--- python/sys_idn.py
+++ python/sys_idn.py
@@ -18,9 +18,19 @@ class system_identification():
     def input_generator(self):
         x = []
 
+        total_progress = len(self.frequencies)*self.generate_periods-1
+        progress = 0
+
+        print("Start system input generation")
         for w in self.frequencies:
+            x_aux = self._one_period(w)
             for T in np.arange(self.generate_periods):
-                x = np.append(x, self._one_period(w))
+                x = np.append(x, x_aux)
+                print (progress,"\t"," of ", total_progress, end="\r")
+                progress += 1
+
+
+        print("\nSaving input to file")
 
         x_file = np.zeros((2,len(x)))
         x_file[0] = np.arange(len(x))
@@ -29,20 +39,38 @@ class system_identification():
         np.savetxt("input.txt",
                    x_file.transpose(), delimiter=" ",
                    header="Sample Amplitude_[a.u.]")
+
+        print("Finish")
+
         return x
 
     def output_processor(self, y, proc_period):
-        position = 0
+        position = 100
         H = []
+
+        print("Start output processing")
+
+        total_progress = len(self.periods)*self.generate_periods-1
+        progress = 0
+
         for N in self.periods:
             for T in np.arange(self.generate_periods):
                 if(T == proc_period):
+                    print(position, position+N)
                     H = np.append(H, np.max(y[position:(position+N)]))
                 position += N
+                #print (progress,"\t"," of ", total_progress, end="\r")
+                #progress += 1
+
+        print("\nSaving frequency response to file")
+
         H_file = np.zeros((2,len(H)))
         H_file[0] = self.frequencies
         H_file[1] = H
+
         np.savetxt("frequency_response.txt",
                    H_file.transpose(), delimiter=" ",
                    header="Frequency_[rad] Amplitude_[a.u.]")
+
+        print("Finish")
         return H
